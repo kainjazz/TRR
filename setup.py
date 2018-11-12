be9e8664b49bd41e220e053730dcb9fa1714a799
kainjazz@gmail.com <trolling1488>
2018-11-12 14:28:12 +0300

    add LICENCE and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="btr",
+    version="0.0.1",
+    author="Kainjazz",
+    author_email="kainjazz@gmail.com",
+    description="Reporter from Behave to TestRail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kainjazz/BTR",
+    keywords=['Behave', 'Testrail', 'Test', 'BDD'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
